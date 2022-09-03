f23414f687a9ab821898319acc38edfdf023a8e6
Mark_Laptop <you@example.com>
2022-09-03 19:40:50 +0100

    Add map_f analysis

--- makePics.py
+++ makePics.py
@@ -2,8 +2,19 @@
 from Solvers import *
 from Examples import *
 from firedrake import *
+from decimal import Decimal
 import matplotlib.pyplot as plt
 import numpy as np
+
+
+def Map_f():
+    E4 = Example_4(eps = 1e-15, Size = 40)
+    epss = 10**(np.arange(-15,1,1,dtype=float))
+    u_e = E4[5]
+    for eps in epss:
+        u_h = Solve_MMAP_STAB_Method(*E4, Order = 2, eps = eps)
+        print("eps: "+str(eps) +", L2: %.5f" % norm(u_e-u_h, "L2"))
+        print("eps: "+str(eps) +", H1: %.5f" % norm(u_e-u_h, "H1"))
 
 def GenLH(Example, Solver, epss):
     L2 = []
