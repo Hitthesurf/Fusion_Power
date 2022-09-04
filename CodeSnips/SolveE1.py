2d6e5172ed6401ab623d9deb8aafa5b52e81ff25
Mark_Laptop <you@example.com>
2022-09-04 16:55:42 +0100

    Update solve E1 example and add update

--- CodeSnips/SolveE1.py
+++ CodeSnips/SolveE1.py
@@ -2,11 +2,11 @@ from Solvers import *
 from Examples import *
 from firedrake import *
 
-E1 = Example_1(al = 2, m=10, eps = 1e-10, Size = 40)
+E1 = Example_1(al = 2, m=10, eps = 1e-10, Size = 160)
 u_e = E1[5]
 u_h = Solve_MMAP_Method(*E1, Order = 2, eps = 1e-10, save = True)
 #Save creates a PVD file with u_h, f, ... in output_graph/
 L2_error = norm(u_h - u_e, "L2")
 H1_error = norm(u_h - u_e, "H1")
-print("LM eps 1e-15 ||u - u_exact||_L2: %.1e" % L2_error)
-print("LM eps 1e-15 ||u - u_exact||_H1: %.1e" % H1_error)
+print("MMAP eps 1e-10 ||u - u_exact||_L2: %.1e" % L2_error)
+print("MMAP eps 1e-10 ||u - u_exact||_H1: %.1e" % H1_error)
