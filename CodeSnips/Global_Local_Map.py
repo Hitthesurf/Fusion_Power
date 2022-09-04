d5cc4044306fc576edecf9f57807dd27f1570c07
Mark_Laptop <you@example.com>
2022-09-04 16:15:51 +0100

    Fix A to B as in paper

--- CodeSnips/Global_Local_Map.py
+++ CodeSnips/Global_Local_Map.py
@@ -1,4 +1,4 @@
 for j in range(d):
     F_Global[Map[j]] += F_Local[j]
     for i in range(d):
-        A_Global_Sparse[Map[j], Map[i]] += A_Local[j, i]
+        B_Global_Sparse[Map[j], Map[i]] += B_Local[j, i]
